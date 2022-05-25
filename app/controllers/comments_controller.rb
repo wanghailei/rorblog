227133f5c534b2261f18cc58d27f00d6b683cf76
wanghailei <wanghailei@gmail.com>
2022-05-25 19:10:53 +0800

    Imported and used Local Time.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
 
         http_basic_authenticate_with name: "WHL", password: "password", only: :destroy
         
+        before_action :set_article
+        
         def create
                 @article = Article.find( params[ :article_id ] )
                 @comment = @article.comments.create( comment_params )
@@ -16,7 +18,12 @@ class CommentsController < ApplicationController
         end
         
         private
+        
         def comment_params
                 params.require( :comment ).permit( :commenter, :body, :status )
-        end     
+        end
+        
+        def set_article
+                @article = Article.find params[ :article_id ]
+        end
 end
