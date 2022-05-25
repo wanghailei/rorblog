9a1f3c3a09f81b1282881cbf97b1dcac73a4f93e
wanghailei <wanghailei@gmail.com>
2022-05-25 20:44:08 +0800

    Tried before_action :set_article. Works well.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,29 @@
 class CommentsController < ApplicationController
 
-        http_basic_authenticate_with name: "WHL", password: "password", only: :destroy
+	http_basic_authenticate_with name: "WHL", password: "password", only: :destroy
         
-        before_action :set_article
+	before_action :set_article
         
-        def create
-                @article = Article.find( params[ :article_id ] )
-                @comment = @article.comments.create( comment_params )
-                redirect_to article_path( @article )
-        end
+	def create
+		# @article = Article.find( params[ :article_id ] )
+		@comment = @article.comments.create!( comment_params )
+		redirect_to article_path( @article )
+	end
         
-        def destroy
-                @article = Article.find( params[ :article_id ] )
-                @comment = @article.comments.find( params[ :id ] )
-                @comment.destroy
-                redirect_to article_path( @article ), status: 303
-        end
+	def destroy
+		# @article = Article.find( params[ :article_id ] )
+		@comment = @article.comments.find( params[ :id ] )
+		@comment.destroy
+		redirect_to article_path( @article ), status: 303
+	end
         
-        private
+	private
         
-        def comment_params
-                params.require( :comment ).permit( :commenter, :body, :status )
-        end
+	def comment_params
+		params.require( :comment ).permit( :commenter, :body, :status )
+	end
         
-        def set_article
-                @article = Article.find params[ :article_id ]
-        end
+	def set_article
+		@article = Article.find params[ :article_id ]
+	end
 end
